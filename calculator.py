abce710eb611d586a1ddec882b1c456ca875d4bc
Yan Ngo <57730149+pyngo05@users.noreply.github.com>
2020-11-19 21:02:02 +0000

    First Version

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title('Basic Calculator')
+root.title('Calculator')
 
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
@@ -11,9 +11,53 @@ def button_click(number):
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
 
-def button_clear():
+def clear():
     e.delete(0, END)
 
+def add():
+    num1 = e.get()
+    global f_num 
+    global math
+    math = 'addition'
+    f_num = int(num1)
+    e.delete(0, END)
+
+def subtract():
+    num1 = e.get()
+    global f_num 
+    global math
+    math = 'subtraction'
+    f_num = int(num1)
+    e.delete(0, END)
+
+def multiply():
+    num1 = e.get()
+    global f_num 
+    global math
+    math = 'multiplication'
+    f_num = int(num1)
+    e.delete(0, END)
+
+def divide():
+    num1 = e.get()
+    global f_num 
+    global math
+    math = 'division'
+    f_num = int(num1)
+    e.delete(0, END)
+
+def equal():
+    num2 = e.get()
+    e.delete(0, END)
+
+    if math == "addition":
+        e.insert(0, f_num + int(num2))
+    elif math == "subtraction":
+        e.insert(0, f_num - int(num2))
+    elif math == "multiplication":
+        e.insert(0, f_num * int(num2))
+    elif math == "division":
+        e.insert(0, f_num / int(num2))
 
 button1 = Button(root, text='1', padx=40, pady=20, command=lambda: button_click(1))
 button2 = Button(root, text='2', padx=40, pady=20, command=lambda: button_click(2))
@@ -25,9 +69,12 @@ button7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click(
 button8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
 button9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
 button0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text='+', padx=39, pady=20, command=lambda: button_click())
-button_equal = Button(root, text='=', padx=91, pady=20, command=lambda: button_click())
-button_clear = Button(root, text='Clear', padx=79, pady=20, command= button_clear)
+button_add = Button(root, text='+', padx=39, pady=20, command= add)
+button_equal = Button(root, text='=', padx=102, pady=20, command= equal)
+button_clear = Button(root, text='Clear', padx=25, pady=15, command= clear)
+button_subtract = Button(root, text='-', padx=41, pady=20, command= subtract)
+button_multiply = Button(root, text='x', padx=40, pady=20, command= multiply)
+button_divide = Button(root, text='/', padx=41, pady=20, command= divide)
 
 button1.grid(row=3, column=0)
 button2.grid(row=3, column=1)
@@ -39,8 +86,11 @@ button7.grid(row=1, column=0)
 button8.grid(row=1, column=1)
 button9.grid(row=1, column=2)
 button0.grid(row=4, column=0)
-button_add.grid(row=5, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_add.grid(row=1, column=3)
+button_clear.grid(row=0, column=3)
+button_equal.grid(row=4, column=1, columnspan=2)
+button_subtract.grid(row=2, column=3)
+button_multiply.grid(row=4, column=3)
+button_divide.grid(row=3, column=3)
 
 root.mainloop()
